9b3876ae46d6279fd26232782e5396fa3ec880f9
Roshad <roshadgu@gmail.com>
2022-12-27 17:49:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/chatgptandroid/screens/Landing Screen.kt
@@ -0,0 +1,10 @@
+package com.example.chatgptandroid.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+
+@Composable
+fun mainScreen()
+{
+    Bo
+}
